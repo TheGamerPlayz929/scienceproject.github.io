c5250ebda90cc57d64a52e2831ec24d3ffef022c
Emirksi <emirbakir523@gmail.com>
2026-05-05 22:05:41 -0400

    Fix GitHub Pages settings loading

--- local-server.js
+++ local-server.js
@@ -49,7 +49,7 @@ function readSiteSettings() {
         ],
       },
       footer: {
-        supportEmail: "emirbakir523@gmail.com and/or thegamerp929@gmail.com",
+        supportEmail: "For all inquiries, support, or removal requests, please contact us at emirbakir523@gmail.com or thegamerp929@gmail.com",
       },
       bellSchedules: {},
       scheduleOverride: null,
